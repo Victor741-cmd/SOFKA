410b0d1be1c476be038368da9565bb55bdf7c2f9
OCHOA <OCHOA@DESKTOP-IU365UJ>
2021-05-17 20:56:30 -0500

    Actualizado

---
+++ src/carrera/Carrera.java
@@ -0,0 +1,62 @@
+package carrera;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+public class Carrera extends Thread{
+     
+    //creamos las variables
+     private JLabel etiqueta;
+     private frmCarrera auto;
+
+     
+    //constructor
+    public Carrera(JLabel etiqueta, frmCarrera auto) {
+        this.etiqueta = etiqueta;
+        this.auto = auto;
+    }
+     
+    
+    @Override
+    public void run(){
+        
+        int auto1 = 0;
+        int auto2 = 0;
+        int auto3 = 0;
+         while(true){
+             try{
+                 
+                  sleep((int)(Math.random() * 1000));
+                  auto1 = auto.getPrimerAuto().getLocation().x;
+                  auto2 = auto.getSegundoAuto().getLocation().x;
+                  auto3 = auto.getTercerAuto().getLocation().x;
+                  
+                  if(auto1 < auto.getMeta().getLocation().x - 125 && auto2 < auto.getMeta().getLocation().x - 125 && auto3 < auto.getMeta().getLocation().x - 125){
+                      etiqueta.setLocation(etiqueta.getLocation().x + 10,etiqueta.getLocation().y);
+                      auto.repaint();
+                  }else{
+                      break;
+                  }
+                  
+             }catch(InterruptedException e){
+                 System.out.println(e);
+             }
+            
+             if(etiqueta.getLocation().x >= auto.getMeta().getLocation().x - 125){
+                  if(auto1 > auto2 && auto1 > auto3){
+                      JOptionPane.showMessageDialog(null,"Ganó el primer carro");
+                  }
+                  else if(auto2 > auto1 && auto2 > auto3){
+                      JOptionPane.showMessageDialog(null,"Gano el segundo carro");
+                  }
+                  else if(auto3 > auto1 && auto3 > auto2 ){
+                      JOptionPane.showMessageDialog(null,"Gano el tercer carro");
+                  }
+                  else{
+                      JOptionPane.showMessageDialog(null,"Empate");
+                  }
+             }
+                   
+        } 
+    }
+}
